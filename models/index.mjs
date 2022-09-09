18cecd533c924e03b76f5213bf9014e6424a4c79
aqifsariman <aqifsariman@gmail.com>
2022-09-09 09:22:22 +0800

    Revert back

--- models/index.mjs
+++ models/index.mjs
@@ -14,42 +14,39 @@ import chargingsModel from './chargings.mjs';
 // import notificationsModel from './notifications.mjs';
 // import transactionsModel from './transactions.mjs';
 
-const env = process.env.ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 
 const db = {};
 let sequelize;
 
 // If env is production, retrieve database auth details from the
 // DATABASE_URL env var that Heroku provides us
-// if (env === 'PRODUCTION') {
-// // Break apart the Heroku database url and rebuild the configs we need
-// const { DATABASE_URL } = process.env;
-// const dbUrl = url.parse(DATABASE_URL);
-// const username = dbUrl.auth.substr(0, dbUrl.auth.indexOf(':'));
-// const password = dbUrl.auth.substr(
-//   dbUrl.auth.indexOf(':') + 1,
-//   dbUrl.auth.length
-// );
-// const dbName = dbUrl.path.slice(1);
-// const host = dbUrl.hostname;
-// const { port } = dbUrl;
-// allConfig.host = host;
-// allConfig.port = port;
-sequelize = new Sequelize('energy_plus', 'postgres', process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-// }
+if (env === 'production') {
+  // Break apart the Heroku database url and rebuild the configs we need
+  const { DATABASE_URL } = process.env;
+  const dbUrl = url.parse(DATABASE_URL);
+  const username = dbUrl.auth.substr(0, dbUrl.auth.indexOf(':'));
+  const password = dbUrl.auth.substr(
+    dbUrl.auth.indexOf(':') + 1,
+    dbUrl.auth.length
+  );
+  const dbName = dbUrl.path.slice(1);
+  const host = dbUrl.hostname;
+  const { port } = dbUrl;
+  allConfig.host = host;
+  allConfig.port = port;
+  sequelize = new Sequelize(dbName, username, password, allConfig);
+}
 
 // If env is not production, retrieve DB auth details from the allConfig
-// else {
-//   sequelize = new Sequelize(
-//     allConfig.database,
-//     allConfig.username,
-//     allConfig.password,
-//     allConfig
-//   );
-// }
+else {
+  sequelize = new Sequelize(
+    allConfig.database,
+    allConfig.username,
+    allConfig.password,
+    allConfig
+  );
+}
 
 // ----- Models Relationship ----- //
 db.User = usersModel(sequelize, Sequelize.DataTypes);
